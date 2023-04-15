e32a05baf9587bc61db5f04926017566b48b0480
Yulien Khlevnyi <yulian.khlevnoy@gmail.com>
2023-04-15 12:07:14 +0200

    Move from Keil uVision to CSS #major
    CSS is now much better to work with.

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+../lcd16x2_msp43x.c \
+../main.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./lcd16x2_msp43x.d \
+./main.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./lcd16x2_msp43x.obj \
+./main.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"lcd16x2_msp43x.obj" \
+"main.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"lcd16x2_msp43x.d" \
+"main.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../lcd16x2_msp43x.c" \
+"../main.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
- //#include <msp432.h>			// seems unnecessary
- #include <stdint.h>
- #include "lcd16x2_msp43x.h"
+#include "msp.h"
+#include <stdint.h>
+#include "lcd16x2_msp43x.h"
 
- int main(void)
- {
+int main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;        // stop watchdog timer
+
     P4->DIR = 0xFF;     // Set P4 pins as output
     P4->OUT = 0x00;     // Clear all pins
 
@@ -14,16 +16,16 @@
 
     lcd16x2_SetPosition( 1, 0 );
     lcd16x2_PrintString("16x2 LCD");
-	 
-	  //lcd16x2_Cmd( LCD_CMD_CLEAR_LCD );
-	  //lcd16x2_Cmd( LCD_CMD_DISPLAY_OFF_CURSOR_OFF );
-	  lcd16x2_Cmd( LCD_CMD_DISPLAY_ON_CURSOR_OFF );
-		//lcd16x2_Cmd( LCD_CMD_DISPLAY_ON_CURSOR_BLINK );
 
-     /* The underscore at the end of printed text isn't any kind of error,
+    //lcd16x2_Cmd( LCD_CMD_CLEAR_LCD );
+    //lcd16x2_Cmd( LCD_CMD_DISPLAY_OFF_CURSOR_OFF );
+    lcd16x2_Cmd( LCD_CMD_DISPLAY_ON_CURSOR_OFF );
+    //lcd16x2_Cmd( LCD_CMD_DISPLAY_ON_CURSOR_BLINK );
+
+    /* The underscore at the end of printed text isn't any kind of error,
         it's just a cursor and it's possible to manipulate its state
         in lcd16x2_msp43x.c or directly from main.c */
 
-     //lcd16x2_Cmd(LCD_CMD_DISPLAY_ON_CURSOR_OFF);     // display ON, cursor OFF/ON/BLINK
-                                                     // lines 101-103 in lcd16x2_msp43x.h
- }
+    //lcd16x2_Cmd(LCD_CMD_DISPLAY_ON_CURSOR_OFF);     // display ON, cursor OFF/ON/BLINK
+    // lines 101-103 in lcd16x2_msp43x.h
+}
